a3cd3d2022df4a560d398edd1bcc61735078f459
Rafael Pereira Dias <p.rafael.dias@viveo.com.br>
2023-11-17 13:56:54 -0300

    created domain project, base entity and user entity

---
+++ CoreDomain/Entities/Base/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿namespace CoreDomain.Entities.Base
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public Guid CreatedBy { get; set; }
+        public Guid CreatedFrom { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public Guid? UpdatedBy { get; set; }
+        public Guid? UpdateReason { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime? DeletedAt { get; set; }
+        public Guid? DeletedBy { get; set; }
+        public Guid? DeleteReason { get; set; }
+    }
+}
---
+++ CoreDomain/Entities/User/UserEntity.cs
@@ -0,0 +1,17 @@
+﻿using CoreDomain.Entities.Base;
+
+namespace CoreDomain.Entities.User
+{
+    public class UserEntity : BaseEntity
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public bool IsEmailConfirmed { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool IsPhoneNumberConfirmed { get; set; }
+    }
+}
