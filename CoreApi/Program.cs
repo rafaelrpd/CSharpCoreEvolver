61509b2f9bb54274fca6da4deee1fef924a000e5
Rafael Pereira Dias <p.rafael.dias@viveo.com.br>
2023-11-17 14:27:31 -0300

    created dbcontext

--- CoreApi/Program.cs
+++ CoreApi/Program.cs
@@ -1,8 +1,12 @@
+using CoreContext;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<CoreDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("CoreDB")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
---
+++ CoreContext/CoreDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CoreDomain.Entities.User;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreContext
+{
+    public class CoreDbContext : DbContext
+    {
+        public CoreDbContext(DbContextOptions<CoreDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<UserEntity> Users { get; set; }
+    }
+}
